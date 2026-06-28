7313108d1f76798f48472d108df550da68820d28
PutilovSy <denisperel2005@gmail.com>
2026-06-28 15:15:05 +0500

    All entyties are ready

--- src/main/java/com/epxense/model/Transaction.java
+++ src/main/java/com/epxense/model/Transaction.java
@@ -2,6 +2,9 @@ package com.epxense.model;
 
 import jakarta.persistence.*;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 @Entity
 @Table(name = "transactions")
 public class Transaction
@@ -10,5 +13,53 @@ public class Transaction
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "")
+    @Column(name = "amount")
+    private BigDecimal amount;
+
+    @Column(name = "timestamp")
+    private LocalDateTime timestamp;
+
+    @Column(name = "event_id")
+    private Event event;
+
+    @ManyToOne
+    @Column(name = "sender_id")
+    private Participant sender;
+
+    @ManyToOne
+    @Column(name = "receiver_id")
+    private Participant receiver;
+
+    public Transaction(){}
+
+    @PrePersist
+    public void onCreate(){
+        if (this.timestamp == null) this.timestamp = LocalDateTime.now();
+    }
+
+    public Long getId() {return id; }
+    public void setId(Long id) {this.id = id; }
+
+    public BigDecimal getAmount() {return amount; }
+
+    public void setAmount(BigDecimal amount) {this.amount = amount; }
+
+    public LocalDateTime getTimestamp() {return timestamp; }
+    public void setTimestamp(LocalDateTime timestamp) {this.timestamp = timestamp; }
+
+    public Event getEvent() {return event; }
+    public void setEvent(Event event) {this.event = event; }
+
+    public Participant getSender() {return sender; }
+    public void setSender(Participant sender) {this.sender = sender; }
+
+    public Participant getReceiver() {return receiver; }
+    public void setReceiver(Participant receiver) {this.receiver = receiver; }
+
+    @Override
+    public String toString() {
+        return "ТРАНЗАКЦИЯ: ID = " + id + ", amount = " + amount +
+                ", date = " + timestamp + ", sender - " + sender +
+                ", receiver - " + receiver;
+    }
 }
---
+++ src/main/java/com/epxense/repository/PurchaseRepository.java
@@ -0,0 +1,12 @@
+package com.epxense.repository;
+
+
+import com.epxense.model.Purchase;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PurchaseRepository extends JpaRepository<Purchase, Long>
+{
+
+}
